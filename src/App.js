3c7a28d0f6c1af798ed29086ce4cf8f36919330f
Williams <williams.dmca@gmail.com>
2022-08-26 06:23:55 -0400

    desafios agregados

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 //import logo from './logo.svg';
 import './App.css';
+import ItemListContainer from './components/ItemListContainer';
+import NavBar from './components/NavBar';
 
 function App() {
+
+  const mensaje = 'Bienvenido a mi app!!'
+  const estilos = {
+    backgroundColor: '#888',
+    padding: '20px'
+  }
+  
   return (
-    <>
-      <h1>hola mundo</h1>
-    </>
+    <div className='container'>
+      <h1>Hola mundo</h1>
+      <strong>otro mensaje</strong>
+      <h3 style={estilos}>{mensaje}</h3>
+      <NavBar></NavBar>
+      <ItemListContainer greeting={'Saludos'}></ItemListContainer>
+    </div>
   );
 }
 
